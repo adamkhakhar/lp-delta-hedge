224983602df367d17385ef641c4971e4e0ff5ba7
ak <ak@aks-MacBook-Pro.lan>
2022-07-12 14:12:52 -0400

    add plot - lppnl

--- src/plot_runner.py
+++ src/plot_runner.py
@@ -6,19 +6,25 @@ sys.path.append(
 )
 import image_creation
 
-PATH_TO_RESULTS = (
-    f"{os.path.dirname(os.path.dirname(os.path.realpath(__file__)))}/results"
-)
+
 IL = lambda x: 2 * (x + 1) ** 0.5 / (x + 2) - 1
 image_creation.create_plot_from_fn(
     IL,
     -1,
     1,
-    y_min=None,
-    y_max=None,
-    save_path=PATH_TO_RESULTS,
     save_title="IL",
     xlabel=r"$\delta$",
     ylabel="Impremanent Loss",
     title="Impermanent Loss",
+)
+
+LPPNL = lambda x: (x + 1) ** 0.5 - 1
+image_creation.create_plot_from_fn(
+    LPPNL,
+    -1,
+    1,
+    save_title="LPPNL",
+    xlabel=r"$\delta$",
+    ylabel="Liquidity Position PNL",
+    title="Liquidity Position PNL",
 )
--- utils/image_creation.py
+++ utils/image_creation.py
@@ -9,14 +9,17 @@ def create_plot_from_fn(
     x_max,
     y_min=None,
     y_max=None,
-    save_path=os.path.dirname(os.path.realpath(__file__)),
+    save_path=f"{os.path.dirname(os.path.dirname(os.path.realpath(__file__)))}/results",
     save_title="fn",
     xlabel=None,
     ylabel=None,
     title=None,
+    x_axis_line=True,
+    shade_pnl=True,
+    num=1_000,
 ):
     fzig, ax = plt.subplots()
-    x = np.linspace(x_min, x_max, num=1_000)
+    x = np.linspace(x_min, x_max, num=num)
     y = [f(x_i) for x_i in x]
     plt.plot(x, y)
     ax.spines["right"].set_color((0.8, 0.8, 0.8))
@@ -25,8 +28,13 @@ def create_plot_from_fn(
     plt.ylabel(ylabel)
     plt.title(title)
 
-    # add more ticks
-    # ax.set_xticks([1, 5, 10, 20, 50, 100])
+    if x_axis_line:
+        plt.axhline(y=0, color="black", linestyle="dotted", alpha=0.25)
+
+    if shade_pnl:
+        zeros = np.zeros(x.shape)
+        ax.fill_between(x, y, zeros, where=(y < zeros), color="darkred", alpha=1 / 12)
+        ax.fill_between(x, y, zeros, where=(y > zeros), color="darkgreen", alpha=1 / 12)
 
     # tweak the axis labels
     xlab = ax.xaxis.get_label()
