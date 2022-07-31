da88f4f645326de4722c13b9767bfde482bf1039
ak <ak@aks-MacBook-Pro.lan>
2022-07-31 02:05:15 -0400

    plot combined pnl as fun of portfolio

--- src/v2_experiment_runner.py
+++ src/v2_experiment_runner.py
@@ -15,14 +15,18 @@ ROOT_DIR = os.path.dirname(BASE_DIR)
 
 if __name__ == "__main__":
     path_to_config = ROOT_DIR + "/configs/uniform_liquidity_config.json"
-    TARGET_FUN = lambda x: -463_647 * (((x / 1613.68) ** 0.5) - 1)
     with open(path_to_config) as f:
         config = json.load(f)
-
+    TARGET_FUN = (
+        lambda x: -1
+        * config["portfolio_initial_value"]
+        * (((x / config["initial_asset_price"]) ** 0.5) - 1)
+    )
     data_params = {
         "final_price_lower_bound": config["final_price_lower_bound"],
         "final_price_upper_bound": config["final_price_upper_bound"],
         "target_function": TARGET_FUN,
+        "portfolio_initial_value": config["portfolio_initial_value"],
     }
     deriv_params = {
         "asset": config["asset"],
